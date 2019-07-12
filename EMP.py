f0e5f24cf4e1436812e726d44a2874eefc27b27d
Nithil A S <nithil1997@gmail.com>
2019-07-12 15:27:31 +0530

    Add files via upload

---
+++ EMP.py
@@ -0,0 +1,16 @@
+'''
+class Emp:
+    first_name= ''
+    last_name= ''
+    dept = 'acting'
+    age = '45'
+    email_domain= 'yahoo.com'
+
+    def mail(self,first_name,last_name):
+        print("EmailID = {}.{}@{}".format(first_name,last_name,self.email_domain))
+
+
+emp1 = Emp()
+emp1.mail("thalapathy","vijay")
+'''    
+
